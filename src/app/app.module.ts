dfb01ab5bccd8324718f1f0857dbb9d8261adb09
bladyamir <mr.amir.1971@gmail.com>
2018-01-16 04:31:41 +0600

    login done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -17,7 +17,9 @@ import { AppRoutingModule } from './app.routing';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators,FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm:FormGroup;
+  constructor(private builder:FormBuilder,private router:Router) { }
 
   ngOnInit() {
+    this.loginForm=this.builder.group({
+      phone_email:[localStorage.getItem("email_phone"),[Validators.email]],
+      password:[localStorage.getItem("password"),[Validators.required]],
+      remember:[""]
+    })
+  }
+
+  login(){
+    // console.log(this.loginForm.value);
+    if(this.loginForm.controls.phone_email.value==="abc@gmail.com" && this.loginForm.controls.password.value==="abc"){
+      if(this.loginForm.controls.remember.value){
+        localStorage.setItem("email_phone",this.loginForm.controls.phone_email.value);
+        localStorage.setItem("password",this.loginForm.controls.password.value);
+      }
+      this.router.navigate(["user_home"]);
+    }
   }
 
 }
