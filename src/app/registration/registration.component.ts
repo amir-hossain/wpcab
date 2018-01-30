987f22f89069f66279e3b6cbc87f9057dc8bb98a
bladyamir <mr.amir.1971@gmail.com>
2018-01-30 19:36:33 +0600

    sprint one done

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Validators,FormGroup,FormBuilder} from "@angular/forms";
-import { AngularFireDatabase } from "angularfire2/database";
+
+import {AngularFireList,AngularFireDatabase} from "angularfire2/database";
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-registration',
@@ -10,14 +12,24 @@ import { AngularFireDatabase } from "angularfire2/database";
 export class RegistrationComponent implements OnInit {
   startDate = new Date(1990, 0, 1);
   registrationForm:FormGroup;
-  constructor(private builder:FormBuilder,private db:AngularFireDatabase) { }
+  fireList :AngularFireList<{}>;
+  dbRef;
+  constructor(private builder:FormBuilder,private db:AngularFireDatabase) {
+    this.dbRef=this.db.database.ref("/users");
+    console.log(this.fireList);
+
+    var starCountRef =  this.db.database.ref('/users');
+starCountRef.on('value', function(snapshot) {
+  console.log(snapshot.val())
+});
+   }
 
   ngOnInit() {
     this.registrationForm=this.builder.group({
       name:["",Validators.required],
       password:["",Validators.required],
       phone:["",Validators.required],
-      role:["",Validators.required],
+      role:["User"],
       area:["",Validators.required],
       district:["",Validators.required],
       upozilla:["",Validators.required],
@@ -26,8 +38,8 @@ export class RegistrationComponent implements OnInit {
       permanentAddress:["",Validators.required],
       age:["",Validators.required],
       invitedBy:["",Validators.required],
-      email:["",[Validators.email]],
-      occupation:[""],
+      email:[""],
+      occupation:["Student"],
       nationality:[""],
       nationalId:[""],
       bloodGroup:[""],
@@ -35,14 +47,23 @@ export class RegistrationComponent implements OnInit {
     })
   }
 
+  
+
   signup(){
     if(this.registrationForm.valid){
-      this.db.list("wpcab-96299").push(this.registrationForm.value);
-      console.log(this.registrationForm.value);
+      let temp=this.registrationForm.value;
+      temp.age=(<Date>this.registrationForm.controls.age.value).toLocaleDateString();
+      this.dbRef.once('value',snap=>{
+      
+          this.dbRef.push(temp);
+          console.log(temp);
+      })
+      // this.registrationForm.reset();
     }else{
       Object.keys(this.registrationForm.controls).forEach(field=>{
         this.registrationForm.get(field).markAsTouched({onlySelf:true});
       })
+    
     }
   
   }
