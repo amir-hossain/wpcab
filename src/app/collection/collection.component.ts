b4d8196d5c8a7f491e25a08493d3a1de20073192
bladyamir <mr.amir.1971@gmail.com>
2018-03-04 04:08:23 +0600

    collection table done

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFireDatabase} from 'angularfire2/database';
+import {MatTableDataSource} from '@angular/material';
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
   styleUrls: ['./collection.component.css']
 })
 export class CollectionComponent implements OnInit {
+
+  displayedColumns = ['name', 'father name', 'mother name'];
+  dataSource;
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    this.dataSource.filter = filterValue;
+  }
   activeUserRole;
   objArry=[];
   constructor(private db:AngularFireDatabase) { }
@@ -19,8 +27,11 @@ export class CollectionComponent implements OnInit {
       console.log(snap.val());
       let data=snap.val();
       Object.keys(data).forEach(key=>this.objArry.push(data[''+key]));
+      this.dataSource = new MatTableDataSource(this.objArry)
+      this.dataSource.filterPredicate = 
+      (data: any, filter: string) => data.fullName.toLowerCase().includes(filter.toLowerCase());
     });
 
   }
-
 }
+
--- src/app/meterial.ts
+++ src/app/meterial.ts
@@ -1,9 +1,9 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule,MatInputModule,MatFormFieldModule,MatCheckboxModule,MatCardModule,MatTabsModule,MatRadioModule,MatDatepickerModule,MatNativeDateModule,MatSnackBarModule,MatAutocompleteModule,MatSelectModule} from '@angular/material'
+import {MatButtonModule,MatInputModule,MatFormFieldModule,MatCheckboxModule,MatCardModule,MatTabsModule,MatRadioModule,MatDatepickerModule,MatNativeDateModule,MatSnackBarModule,MatAutocompleteModule,MatSelectModule,MatTableModule} from '@angular/material'
 @NgModule({
-    imports:[MatButtonModule,MatInputModule,MatFormFieldModule,MatCheckboxModule,MatCardModule,MatTabsModule,MatRadioModule,MatDatepickerModule,MatNativeDateModule,MatSnackBarModule,MatAutocompleteModule,MatSelectModule],
+    imports:[MatButtonModule,MatInputModule,MatFormFieldModule,MatCheckboxModule,MatCardModule,MatTabsModule,MatRadioModule,MatDatepickerModule,MatNativeDateModule,MatSnackBarModule,MatAutocompleteModule,MatSelectModule,MatTableModule],
 
 
-    exports:[MatButtonModule,MatInputModule,MatFormFieldModule,MatCheckboxModule,MatCardModule,MatTabsModule,MatRadioModule,MatDatepickerModule,MatNativeDateModule,MatSnackBarModule,MatAutocompleteModule,MatSelectModule]
+    exports:[MatButtonModule,MatInputModule,MatFormFieldModule,MatCheckboxModule,MatCardModule,MatTabsModule,MatRadioModule,MatDatepickerModule,MatNativeDateModule,MatSnackBarModule,MatAutocompleteModule,MatSelectModule,MatTableModule]
 })
 export class Meterial{}
