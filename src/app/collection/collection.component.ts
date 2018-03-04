114438e734b664078baadc02f3c79be6182ad3ee
bladyamir <mr.amir.1971@gmail.com>
2018-03-05 03:27:25 +0600

    collection page done

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -9,6 +9,7 @@ import {AngularFireDatabase} from 'angularfire2/database';
 export class CollectionComponent implements OnInit {
   activeUserRole;
   objArry=[];
+  filteredArry=[];
   constructor(private db:AngularFireDatabase) { }
 
   ngOnInit() {
@@ -20,8 +21,32 @@ export class CollectionComponent implements OnInit {
       console.log(snap.val());
       let data=snap.val();
       Object.keys(data).forEach(key=>this.objArry.push(data[''+key]));
+  this.filteredArry=this.objArry;
     });
+  }
 
+  nameFilter(name:string){
+    // console.log(name);
+    this.filteredArry=this.objArry.filter(val=>
+    val.fullName.toLowerCase().includes(name.toLowerCase()))
+  }
+
+  fatherNameFilter(fatherName:string){
+    let bakFilteredArray=this.filteredArry.slice();
+    this.filteredArry=this.objArry.filter(val=>
+      val.fatherName.toLowerCase().includes(fatherName.toLowerCase()))
+   
+    }
+
+    motherNameFilter(motherName:string){
+      let bakFilteredArray=this.filteredArry.slice();
+      this.filteredArry=this.objArry.filter(val=>
+        val.motherName.toLowerCase().includes(motherName.toLowerCase()))
+     
+      }
+
+    // console.log(bakFilteredArray);
+    
   }
 }
 
