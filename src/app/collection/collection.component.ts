e4f5334123f16777f9d675ca08d5f146cd969616
bladyamir <mr.amir.1971@gmail.com>
2018-03-04 18:03:09 +0600

    collection page half done

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,19 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFireDatabase} from 'angularfire2/database';
-import {MatTableDataSource} from '@angular/material';
+
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
   styleUrls: ['./collection.component.css']
 })
 export class CollectionComponent implements OnInit {
-
-  displayedColumns = ['name', 'father name', 'mother name'];
-  dataSource;
-  applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); // Remove whitespace
-    this.dataSource.filter = filterValue;
-  }
   activeUserRole;
   objArry=[];
   constructor(private db:AngularFireDatabase) { }
@@ -27,9 +20,6 @@ export class CollectionComponent implements OnInit {
       console.log(snap.val());
       let data=snap.val();
       Object.keys(data).forEach(key=>this.objArry.push(data[''+key]));
-      this.dataSource = new MatTableDataSource(this.objArry)
-      this.dataSource.filterPredicate = 
-      (data: any, filter: string) => data.fullName.toLowerCase().includes(filter.toLowerCase());
     });
 
   }
